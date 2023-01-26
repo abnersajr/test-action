d3bf776d33a886de7e0ef227fb568c7c43911f78
Abner Soares Alves Junior <abnersajr@gmail.com>
2023-01-26 01:21:18 -0500

    wip

--- script/check-xps.ts
+++ script/check-xps.ts
@@ -1,6 +1,7 @@
+const path = require("path")
 const files_list = process.argv.slice(2);
 
 files_list.forEach(file => {
-  const xp = require(`./${file}`);
+  const xp = require(path.join(__dirname, file));
   console.log(xp);
 })
