dd0fc6eeb31f81dd19d2cb357e39ad32b9604179
Abner Soares Alves Junior <abnersajr@gmail.com>
2023-01-25 23:19:58 -0500

    feat: adds cart-v2 xp

---
+++ app/lib/experiments/cart-v2.ts
@@ -0,0 +1,38 @@
+const experiment = {
+  id: "iHUPdK1bRdSSmjjScnO5Q",
+  name: "test-xp",
+
+  // Turn rotation to `true` when you want to activate the XP for users in prod.
+  rotate: false,
+
+  // Learn more about rotate preconditions: https://docs.busbud-int.com/pubweb/Experiments/#rotate-preconditions
+  rotate_precondition: {
+    product: { $eq: "test" },
+    user_country: { $eq: "US" }
+  },
+
+  distribution: {
+    "0": "control_1", // 49.5%
+    "495": "control_2", // 49.5%
+    "990": "1" // 1%
+  },
+
+  // `control` group catches people rejected by XP preconditions.
+  // `control_1` and `control_2` are rotating with variants to compare the impact.
+  feature_flags: {
+    control: {
+      CART_V2: false
+    },
+    control_1: {
+      CART_V2: false
+    },
+    control_2: {
+      CART_V2: false
+    },
+    "1": {
+      CART_V2: true
+    }
+  }
+};
+
+export default experiment;
